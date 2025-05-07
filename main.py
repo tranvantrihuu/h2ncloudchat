b432c966fa5c84d050d42443ea8a8baf4508986c
Trần Văn Trí Hửu <148695715+tranvantrihuu@users.noreply.github.com>
2025-05-07 17:41:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -221,5 +221,6 @@ def inject_user():
             return {}
     return {}
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
